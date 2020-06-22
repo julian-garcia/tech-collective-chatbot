55260c77d8a9371aecc20e80f3661d0ba6cb887e
Julian Garcia <julian.garcia.leoni@gmail.com>
2020-06-22 01:33:16 +0100

    Building + styling landing page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Build your own chatbot`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -15,16 +15,17 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `build-chatbot`,
+        short_name: `chatbot`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo-small.svg`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,60 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import { IoIosArrowBack } from 'react-icons/io';
+import exampleImg from "../images/example.png"
+import chatbotImg from "../images/chatbot.png"
+import codeImg from "../images/code.svg"
+import conceptsImg from "../images/concepts.png"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <h1 className="site-title text-center">Build your own chatbot with just enough JavaScript</h1>
+    <a href="/" className="button example colour-5-bg">Live&nbsp;Example</a>
+    <a href="/" className="button colour-1-bg" style={{margin:'0 auto 2rem'}}>Get&nbsp;Started</a>
+    <div className="chat">
+      <div className="bubble left">Would you like to learn how to code?</div>
+      <div className="bubble right">I would, but it's so hard!</div>
+      <div className="bubble right"><span role="img" aria-label="">🤯</span> Where do I start?</div>
+      <div className="bubble right"><span role="img" aria-label="">🤯</span> Why does it feel so abstract?</div>
+      <div className="bubble right"><span role="img" aria-label="">🤯</span> Why can't I understand any of it?</div>
+      <div className="bubble left">You're right, it's hard...</div>
+      <div className="bubble left">But we can make it easier!</div>
+      <div className="bubble left"><span role="img" aria-label="">💪🧠</span> We just need to use some educational research and neuroscience <span role="img" aria-label="">🧠💪</span></div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <h2 className="page-heading">In this tutorial, we'll learn some JavaScript, following these concepts for effective learning.</h2>
+    <section className="angled-block right short">
+      <img src={exampleImg} className="chatbot-image" alt="" />
+    </section>
+    <section className="angled-block left">
+      <span className="number">1</span>
+      <img src={chatbotImg} className="image" alt="" />
+      <div className="arrow left"><IoIosArrowBack /></div>
+      <div className="text">
+        <h3>Build something real</h3>
+        <p>We'll use concrete examples to build a chatbot</p>
+      </div>
+    </section>
+    <section className="angled-block right">
+      <span className="number">2</span>
+      <img src={codeImg} className="image" alt="" />
+      <div className="arrow right"><IoIosArrowBack /></div>
+      <div className="text">
+        <h3>Write actual code</h3>
+        <p>We'll manage cognitive load by learning just enough JavaScript</p>
+      </div>
+    </section>
+    <section className="angled-block left">
+      <span className="number">3</span>
+      <img src={conceptsImg} className="image" alt="" />
+      <div className="arrow left"><IoIosArrowBack /></div>
+      <div className="text">
+        <h3>Understand the concepts</h3>
+        <p>We'll use (micro) interleaving to link together ideas</p>
+      </div>
+    </section>
+    
   </Layout>
 )
 
